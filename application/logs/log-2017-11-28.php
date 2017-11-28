e1ddc2ad45c87d4b494fbf9b47419301640cfb56
tamil.oakdev@gmail.com <tamil.oakdev@gmail.com>
2017-11-29 00:58:37 +0530

    testing

---
+++ application/logs/log-2017-11-28.php
@@ -0,0 +1,130 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2017-11-28 20:21:04 --> Config Class Initialized
+INFO - 2017-11-28 20:21:04 --> Hooks Class Initialized
+DEBUG - 2017-11-28 20:21:04 --> UTF-8 Support Enabled
+INFO - 2017-11-28 20:21:04 --> Utf8 Class Initialized
+INFO - 2017-11-28 20:21:04 --> URI Class Initialized
+DEBUG - 2017-11-28 20:21:04 --> No URI present. Default controller set.
+INFO - 2017-11-28 20:21:04 --> Router Class Initialized
+INFO - 2017-11-28 20:21:04 --> Output Class Initialized
+INFO - 2017-11-28 20:21:04 --> Security Class Initialized
+DEBUG - 2017-11-28 20:21:04 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-11-28 20:21:05 --> Input Class Initialized
+INFO - 2017-11-28 20:21:05 --> Language Class Initialized
+INFO - 2017-11-28 20:21:05 --> Loader Class Initialized
+INFO - 2017-11-28 20:21:05 --> Controller Class Initialized
+INFO - 2017-11-28 20:21:05 --> Helper loaded: form_helper
+INFO - 2017-11-28 20:21:05 --> Helper loaded: url_helper
+INFO - 2017-11-28 20:21:05 --> Form Validation Class Initialized
+INFO - 2017-11-28 20:21:05 --> Model Class Initialized
+DEBUG - 2017-11-28 20:21:05 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2017-11-28 20:21:05 --> Session: Class initialized using 'files' driver.
+INFO - 2017-11-28 20:21:05 --> Database Driver Class Initialized
+DEBUG - 2017-11-28 20:21:05 --> Session class already loaded. Second attempt ignored.
+INFO - 2017-11-28 20:21:05 --> Helper loaded: admission_helper
+INFO - 2017-11-28 20:21:05 --> File loaded: C:\xampp\htdocs\git_erp\application\views\admission/new.php
+INFO - 2017-11-28 20:21:05 --> Final output sent to browser
+DEBUG - 2017-11-28 20:21:05 --> Total execution time: 0.2780
+INFO - 2017-11-28 20:21:18 --> Config Class Initialized
+INFO - 2017-11-28 20:21:18 --> Hooks Class Initialized
+DEBUG - 2017-11-28 20:21:18 --> UTF-8 Support Enabled
+INFO - 2017-11-28 20:21:18 --> Utf8 Class Initialized
+INFO - 2017-11-28 20:21:18 --> URI Class Initialized
+INFO - 2017-11-28 20:21:18 --> Router Class Initialized
+INFO - 2017-11-28 20:21:18 --> Output Class Initialized
+INFO - 2017-11-28 20:21:18 --> Security Class Initialized
+DEBUG - 2017-11-28 20:21:18 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-11-28 20:21:18 --> Input Class Initialized
+INFO - 2017-11-28 20:21:18 --> Language Class Initialized
+ERROR - 2017-11-28 20:21:18 --> 404 Page Not Found: Employee/index
+INFO - 2017-11-28 20:21:25 --> Config Class Initialized
+INFO - 2017-11-28 20:21:25 --> Hooks Class Initialized
+DEBUG - 2017-11-28 20:21:25 --> UTF-8 Support Enabled
+INFO - 2017-11-28 20:21:25 --> Utf8 Class Initialized
+INFO - 2017-11-28 20:21:25 --> URI Class Initialized
+INFO - 2017-11-28 20:21:25 --> Router Class Initialized
+INFO - 2017-11-28 20:21:25 --> Output Class Initialized
+INFO - 2017-11-28 20:21:25 --> Security Class Initialized
+DEBUG - 2017-11-28 20:21:25 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-11-28 20:21:25 --> Input Class Initialized
+INFO - 2017-11-28 20:21:25 --> Language Class Initialized
+ERROR - 2017-11-28 20:21:25 --> 404 Page Not Found: Employeeimpl/index
+INFO - 2017-11-28 20:21:30 --> Config Class Initialized
+INFO - 2017-11-28 20:21:30 --> Hooks Class Initialized
+DEBUG - 2017-11-28 20:21:30 --> UTF-8 Support Enabled
+INFO - 2017-11-28 20:21:30 --> Utf8 Class Initialized
+INFO - 2017-11-28 20:21:30 --> URI Class Initialized
+DEBUG - 2017-11-28 20:21:30 --> No URI present. Default controller set.
+INFO - 2017-11-28 20:21:30 --> Router Class Initialized
+INFO - 2017-11-28 20:21:30 --> Output Class Initialized
+INFO - 2017-11-28 20:21:30 --> Security Class Initialized
+DEBUG - 2017-11-28 20:21:30 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-11-28 20:21:30 --> Input Class Initialized
+INFO - 2017-11-28 20:21:30 --> Language Class Initialized
+INFO - 2017-11-28 20:21:30 --> Loader Class Initialized
+INFO - 2017-11-28 20:21:30 --> Controller Class Initialized
+INFO - 2017-11-28 20:21:30 --> Helper loaded: form_helper
+INFO - 2017-11-28 20:21:30 --> Helper loaded: url_helper
+INFO - 2017-11-28 20:21:30 --> Form Validation Class Initialized
+INFO - 2017-11-28 20:21:30 --> Model Class Initialized
+DEBUG - 2017-11-28 20:21:30 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2017-11-28 20:21:30 --> Session: Class initialized using 'files' driver.
+INFO - 2017-11-28 20:21:30 --> Database Driver Class Initialized
+DEBUG - 2017-11-28 20:21:30 --> Session class already loaded. Second attempt ignored.
+INFO - 2017-11-28 20:21:30 --> Helper loaded: admission_helper
+INFO - 2017-11-28 20:21:30 --> File loaded: C:\xampp\htdocs\git_erp\application\views\admission/new.php
+INFO - 2017-11-28 20:21:30 --> Final output sent to browser
+DEBUG - 2017-11-28 20:21:30 --> Total execution time: 0.1154
+INFO - 2017-11-28 20:22:05 --> Config Class Initialized
+INFO - 2017-11-28 20:22:05 --> Hooks Class Initialized
+DEBUG - 2017-11-28 20:22:05 --> UTF-8 Support Enabled
+INFO - 2017-11-28 20:22:05 --> Utf8 Class Initialized
+INFO - 2017-11-28 20:22:05 --> URI Class Initialized
+DEBUG - 2017-11-28 20:22:05 --> No URI present. Default controller set.
+INFO - 2017-11-28 20:22:05 --> Router Class Initialized
+INFO - 2017-11-28 20:22:05 --> Output Class Initialized
+INFO - 2017-11-28 20:22:05 --> Security Class Initialized
+DEBUG - 2017-11-28 20:22:05 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-11-28 20:22:05 --> Input Class Initialized
+INFO - 2017-11-28 20:22:05 --> Language Class Initialized
+INFO - 2017-11-28 20:22:05 --> Loader Class Initialized
+INFO - 2017-11-28 20:22:05 --> Controller Class Initialized
+INFO - 2017-11-28 20:22:05 --> Helper loaded: form_helper
+INFO - 2017-11-28 20:22:05 --> Helper loaded: url_helper
+INFO - 2017-11-28 20:22:05 --> Form Validation Class Initialized
+INFO - 2017-11-28 20:22:05 --> Model Class Initialized
+DEBUG - 2017-11-28 20:22:05 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2017-11-28 20:22:05 --> Session: Class initialized using 'files' driver.
+INFO - 2017-11-28 20:22:05 --> Database Driver Class Initialized
+DEBUG - 2017-11-28 20:22:05 --> Session class already loaded. Second attempt ignored.
+INFO - 2017-11-28 20:22:05 --> Helper loaded: admission_helper
+INFO - 2017-11-28 20:22:05 --> File loaded: C:\xampp\htdocs\git_erp\application\views\admission/new.php
+INFO - 2017-11-28 20:22:05 --> Final output sent to browser
+DEBUG - 2017-11-28 20:22:05 --> Total execution time: 0.1125
+INFO - 2017-11-28 20:22:09 --> Config Class Initialized
+INFO - 2017-11-28 20:22:09 --> Hooks Class Initialized
+DEBUG - 2017-11-28 20:22:09 --> UTF-8 Support Enabled
+INFO - 2017-11-28 20:22:09 --> Utf8 Class Initialized
+INFO - 2017-11-28 20:22:09 --> URI Class Initialized
+DEBUG - 2017-11-28 20:22:09 --> No URI present. Default controller set.
+INFO - 2017-11-28 20:22:09 --> Router Class Initialized
+INFO - 2017-11-28 20:22:09 --> Output Class Initialized
+INFO - 2017-11-28 20:22:09 --> Security Class Initialized
+DEBUG - 2017-11-28 20:22:10 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-11-28 20:22:10 --> Input Class Initialized
+INFO - 2017-11-28 20:22:10 --> Language Class Initialized
+INFO - 2017-11-28 20:22:10 --> Loader Class Initialized
+INFO - 2017-11-28 20:22:10 --> Controller Class Initialized
+INFO - 2017-11-28 20:22:10 --> Helper loaded: form_helper
+INFO - 2017-11-28 20:22:10 --> Helper loaded: url_helper
+INFO - 2017-11-28 20:22:10 --> Form Validation Class Initialized
+INFO - 2017-11-28 20:22:10 --> Model Class Initialized
+DEBUG - 2017-11-28 20:22:10 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2017-11-28 20:22:10 --> Session: Class initialized using 'files' driver.
+INFO - 2017-11-28 20:22:10 --> Database Driver Class Initialized
+DEBUG - 2017-11-28 20:22:10 --> Session class already loaded. Second attempt ignored.
+INFO - 2017-11-28 20:22:10 --> Helper loaded: admission_helper
+INFO - 2017-11-28 20:22:10 --> File loaded: C:\xampp\htdocs\git_erp\application\views\admission/new.php
+INFO - 2017-11-28 20:22:10 --> Final output sent to browser
+DEBUG - 2017-11-28 20:22:10 --> Total execution time: 0.1262
